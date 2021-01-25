5df97eea39987c8bc51d91e745f33cf974780b35
Mariusz Krawczyk <przyslony@gmail.com>
2021-01-25 17:10:35 +0000

    fix bug windows is undefine

--- src/utility/useWindowWidht.js
+++ src/utility/useWindowWidht.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react'
 
 const useWindowWidth = () => {
+  if (typeof window === 'undefined') return;
   const [width, setWidth] = useState(window.innerWidth)
 
   useEffect(() => {
-    if (typeof window.innerWidth === 'undefined') return;
     
     const handleResize = () => setWidth(window.innerWidth);
     window.addEventListener('resize', handleResize);
